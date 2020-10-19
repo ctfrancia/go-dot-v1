56fe8149181adaf1400bc71d9372f5b5de485bfe
Christian Francia <ctfrancia@gmail.com>
2020-10-19 23:18:47 +0200

    add basic line

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ctfrancia/go-dot
 go 1.14
 
 require (
-	github.com/google/go-github/v32 v32.1.0 // indirect
+	github.com/google/go-github/v32 v32.1.0
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 )
--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	// "github.com/google/go-github/v32/github"
+	"io/ioutil"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
+	dat, err := ioutil.ReadFile("/certs/github")
+	check(err)
+
+	fmt.Println(dat)
 }
